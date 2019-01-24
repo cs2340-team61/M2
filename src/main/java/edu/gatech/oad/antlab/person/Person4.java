e06a34b2078948465ab96aa57d976afa9b5c36ec
Neil Patel <neilpatel10@yahoo.com>
2019-01-24 00:11:21 -0500

    works now

--- src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -34,7 +34,7 @@ public class Person4 {
       String newString = "";
       
       for(int i = 0; i < input.length(); i++) {
-          newString += (input.charAt(i) + 1);
+          newString += (char) (input.charAt(i) + 1);
       }
       return newString;
     }
